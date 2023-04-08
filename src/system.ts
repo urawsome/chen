28a3d3d01570c6e4b2c3810bb4a851c8ee919f10
urawsome <127172070+urawsome@users.noreply.github.com>
2023-04-08 19:33:12 +0800

    Add files via upload
    上传

---
+++ src/system.ts
@@ -0,0 +1,29 @@
+import type { Model } from "./types"
+
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  password: "",
+  systemRule: "",
+  model: "gpt-3.5-turbo" as Model
+}
+
+export const defaultMessage = `
+本站由 ChatGPT 基于 OpenAI GPT-3.5 Turbo API 接口搭建。
+如果你想和它聊天，请先联系 ChatGPT，购买 OpenAI API Key 密钥并填写 (用户密钥均保存在用户本地，不涉及任何上传功能)。
+
+[[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
+
+export type Setting = typeof defaultSetting
+
+export const defaultResetContinuousDialogue = false
+
+export const defaultMaxInputTokens: Record<Model, number> = {
+  "gpt-3.5-turbo": 3072,
+  "gpt-4": 6144,
+  "gpt-4-32k": 24576
+}
+
+export const defaultModel: Model = "gpt-3.5-turbo"
